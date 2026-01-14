7511e50b36dea8a790659531a649554e0d8659b0
ttaebari <taehotuna@naver.com>
2026-01-14 17:42:09 +0900

    feat : 안내 모달 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { routes, RouteConfig } from "./route";
 
+import ConfirmModal from "@/components/modals/ConfirmModal";
+import AlertModal from "@/components/modals/AlertModal";
+
 function App() {
     return (
         <Router>
-            <Routes>
-                {routes.map((route: RouteConfig, index: number) => (
-                    <Route key={index} path={route.path} element={route.element} />
-                ))}
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
+            <div className="App">
+                <ConfirmModal />
+                <AlertModal />
+                <Routes>
+                    {routes.map((route: RouteConfig, index: number) => (
+                        <Route key={index} path={route.path} element={route.element} />
+                    ))}
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </div>
         </Router>
     );
 }
